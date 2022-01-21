452548e36005075bccf60f0e9d03f76fdc1f648e
ishandeshpande <63430023+ishandeshpande@users.noreply.github.com>
2022-01-21 14:51:40 -0800

    Unit 01 Reupload

---
+++ Unit01/src/StarsRunner.java
@@ -0,0 +1,26 @@
+//© A+ Computer Science
+//www.apluscompsci.com
+
+//Name - Ishan Deshpande
+//Date - 1/18/22
+//Class - Period 1 CSA
+//Lab  - StarsRunner
+
+import static java.lang.System.*;
+
+public class StarsRunner
+{
+   public static void main(String args[])
+   {
+    StarsAndStripes patriot = new StarsAndStripes();
+    patriot.printTwentyStars();
+    patriot.printTwentyDashes();
+
+    patriot.printTwentyStars();
+    patriot.printTwentyDashes();
+
+    patriot.printTwentyStars();
+    patriot.printTwentyDashes();
+
+   }
+}
