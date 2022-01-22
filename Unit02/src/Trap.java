54f12ed4e17d4385633693ea4848aa4e13a87d71
ishandeshpande <63430023+ishandeshpande@users.noreply.github.com>
2022-01-21 18:28:23 -0800

    Unit 02

---
+++ Unit02/src/Trap.java
@@ -0,0 +1,13 @@
+//� A+ Computer Science  -  www.apluscompsci.com
+//Name - Ishan Deshpande
+//Date - 1/21/2022
+//Class - AP CSA Period 1
+//Lab  - Trap
+
+public class Trap
+{
+	public static double area( int base1, int base2, int height )
+	{
+		return (double)(0.5*(base1+base2)*height);
+	}
+}
